ec20cc91b1b0120aae736ec9c3906bf35bb9a864
FreePass-Ec-Company <ruthi.qui@gmail.com>
2025-07-02 17:06:24 -0500

    Update app-fotos.js

--- app-fotos.js
+++ app-fotos.js
@@ -1,5 +1,8 @@
 const main_img = document.querySelector('.main_img')
 const thumbnails = document.querySelectorAll('.thumbnail')
+const main_img1 = document.querySelector('.main_img1')
+const main_img2 = document.querySelector('.main_img2')
+const main_img3 = document.querySelector('.main_img3')
 
 thumbnails.forEach(thumb => {
     thumb.addEventListener('click', function(){
@@ -7,5 +10,8 @@ thumbnails.forEach(thumb => {
         active.classList.remove('active')
         this.classList.add('active')
         main_img.src = this.src
+        main_img1.src = this.src
+        main_img2.src = this.src
+        main_img3.src = this.src
     })
 })
